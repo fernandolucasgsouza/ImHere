acce90429f73fdd681e421db6b631216fb4ac7cd
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2024-03-19 21:56:34 -0300

    add TextInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
@@ -6,6 +6,11 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Segunda 18 de Março 2024</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,19 +1,28 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#131016",
-      padding: 24,
-    },
-    eventName: {
-      color: "#fdfcfe",
-      fontSize: 24,
-      fontWeight: "bold",
-      marginTop: 48,
-    },
-    eventDate: {
-      color: "#6b6b6b",
-      fontSize: 16,
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    height: 55,
+    borderRadius: 5,
+    backgroundColor: "#1f1e25",
+    color: "#fff",
+    padding: 15,
+    fontSize: 16,
+    marginVertical: 8,
+  },
+});
