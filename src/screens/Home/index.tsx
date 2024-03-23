09d5f8233b29f945410063b884fa0159122d18f8
fernandolucasgsouza <fernandolucass.2012@gmail.com>
2024-03-23 11:53:33 -0300

    implmentação da remoção do participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -13,19 +13,33 @@ import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
-  const  [participantsState, setParticpantsState] = useState<string[]>([]);
-  const  [participantNameState, setParticpantNameState] = useState<string>('');
-  
+  const [participantsState, setParticpantsState] = useState<string[]>([]);
+  const [participantNameState, setParticpantNameState] = useState<string>("");
+
   function handlerParticipantAdd() {
-    if (participantNameState == '') 
-      return  
-    if(participantsState.includes(participantNameState))
-      return Alert.alert(`${participantNameState} já está cadastrado na lista!`)
-      setParticpantsState(prevState => [...prevState, participantNameState])
-    setParticpantNameState('');
+    if (participantNameState == "") return;
+    if (participantsState.includes(participantNameState))
+      return Alert.alert(
+        `${participantNameState} já está cadastrado na lista!`
+      );
+    setParticpantsState((prevState) => [...prevState, participantNameState]);
+    setParticpantNameState("");
   }
+
   function handlerParticipantRemove(id: number, name: string) {
-    console.log(`clicou em remove o participante: ${name} do id: ${id}`);
+    Alert.alert("Remover", `Deseja remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () =>
+          setParticpantsState((prevState) =>
+            prevState.filter((_, index) => id !== index)
+          ),
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
   }
 
   return (
@@ -48,7 +62,6 @@ export function Home() {
       <FlatList
         data={participantsState}
         keyExtractor={(item) => item}
-        showsVerticalScrollIndicator={false}
         renderItem={({ item, index }) => (
           <Participant
             key={index}
@@ -57,10 +70,11 @@ export function Home() {
             onRemove={(index) => handlerParticipantRemove(index, item)}
           />
         )}
+        showsVerticalScrollIndicator={false}
         ListEmptyComponent={() => (
           <Text style={styles.listEmptyText}>
-            Niguem chegou ao evento?{"\n"} Adicione participantes a sua lista de
-            presença!
+            Ninguém chegou ao evento?{"\n"}
+            Adicione participantes a sua lista de presença!
           </Text>
         )}
       />
